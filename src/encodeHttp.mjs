b467c90b564f6cebf87ab99d920894e04befa673
quan <quan_xiaoxiao@qq.com>
2024-06-21 09:41:52 +0800

    xxx

--- src/encodeHttp.mjs
+++ src/encodeHttp.mjs
@@ -12,10 +12,20 @@ import encodeHttpStartLine from './encodeHttpStartLine.mjs';
 const crlf = Buffer.from('\r\n');
 const BODY_CHUNK_END = Buffer.from('0\r\n\r\n');
 
+const isBodyStream = (options) => options.body && options.body instanceof Readable;
+
+const isChunkEmpty = (chunk) => {
+  if (chunk == null) {
+    return true;
+  }
+  return chunk.length === 0;
+};
+
 export default (options) => {
   const state = {
     complete: false,
     contentSize: 0,
+    contentLength: null,
   };
 
   if (options.headers) {
@@ -27,9 +37,8 @@ export default (options) => {
   assert(httpHeaderList.length % 2 === 0);
 
   const hasBody = Object.hasOwnProperty.call(options, 'body');
-  const isBodyStream = options.body && (options.body instanceof Readable);
 
-  if (hasBody && !isBodyStream && options.body != null) {
+  if (hasBody && options.body != null && !isBodyStream(options)) {
     assert(Buffer.isBuffer(options.body) || typeof options.body === 'string');
   }
 
@@ -44,7 +53,7 @@ export default (options) => {
     options.onStartLine(startlineBuf);
   }
 
-  if (isBodyStream) {
+  if (isBodyStream(options)) {
     keyValuePairList.push('Transfer-Encoding');
     keyValuePairList.push('chunked');
   } else if (hasBody) {
@@ -66,7 +75,7 @@ export default (options) => {
         headersBuf,
       ]));
     }
-    if (!isBodyStream) {
+    if (!isBodyStream(options)) {
       const bufList = [
         startlineBuf,
         crlf,
@@ -87,13 +96,14 @@ export default (options) => {
     if (data != null) {
       assert(Buffer.isBuffer(data) || typeof data === 'string');
     }
+
     const chunk = data != null ? Buffer.from(data) : null;
 
-    if (!chunk || chunk.length === 0) {
+    if (isChunkEmpty(chunk)) {
       state.complete = true;
       if (state.contentSize === 0) {
         if (options.onHeader) {
-          if (isBodyStream) {
+          if (isBodyStream(options)) {
             if (options.onEnd) {
               options.onEnd(state.contentSize);
             }
@@ -112,7 +122,7 @@ export default (options) => {
           crlf,
           encodeHttpHeaders(keyValuePairList),
           crlf,
-          ...isBodyStream ? [BODY_CHUNK_END] : [],
+          ...isBodyStream(options) ? [BODY_CHUNK_END] : [],
         ]);
       }
       if (options.onEnd) {
@@ -124,7 +134,7 @@ export default (options) => {
     const chunkSize = chunk.length;
     if (state.contentSize === 0) {
       state.contentSize = chunkSize;
-      if (isBodyStream) {
+      if (isBodyStream(options)) {
         if (!options.onHeader) {
           return Buffer.concat([
             ...options.onStartLine ? [] : [startlineBuf, crlf],
