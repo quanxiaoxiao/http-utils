21bebe36c75253f78c341d68943a1c6b26a6d843
quan <quan_xiaoxiao@qq.com>
2024-08-01 09:11:58 +0800

    xxx

--- src/wrapContentChunk.mjs
+++ src/wrapContentChunk.mjs
@@ -4,6 +4,11 @@ import assert from 'node:assert';
 const crlf = Buffer.from('\r\n');
 const MAX_CHUNK_SIZE = 65535;
 
+const encodeSize = (n) => {
+  assert(n >= 0 && n <= MAX_CHUNK_SIZE);
+  return Buffer.from(n.toString(16));
+};
+
 export default (chunk) => {
   assert(Buffer.isBuffer(chunk));
   const size = chunk.length;
@@ -12,19 +17,22 @@ export default (chunk) => {
     const remain = size - n * MAX_CHUNK_SIZE;
     const result = [];
     for (let i = 0; i < n; i++) {
-      result.push(Buffer.from('ffff\r\n'));
+      result.push(encodeSize(MAX_CHUNK_SIZE));
+      result.push(crlf);
       result.push(chunk.slice(i * MAX_CHUNK_SIZE, (i + 1) * MAX_CHUNK_SIZE));
       result.push(crlf);
     }
     if (remain !== 0) {
-      result.push(Buffer.from(`${remain.toString(16)}\r\n`));
+      result.push(encodeSize(remain));
+      result.push(crlf);
       result.push(chunk.slice(n * MAX_CHUNK_SIZE));
       result.push(crlf);
     }
     return Buffer.concat(result);
   }
   return Buffer.concat([
-    Buffer.from(`${size.toString(16)}\r\n`),
+    encodeSize(size),
+    crlf,
     chunk,
     crlf,
   ]);
