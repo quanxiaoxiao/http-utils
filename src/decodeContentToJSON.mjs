79f40df6c61a7e79a9af1140105df0782d108a2b
quan <quan_xiaoxiao@qq.com>
2025-04-24 11:30:22 +0800

    xxx

--- src/decodeContentToJSON.mjs
+++ src/decodeContentToJSON.mjs
@@ -4,21 +4,25 @@ import qs from 'node:querystring';
 import _ from 'lodash';
 
 import decodeContentEncoding from './decodeContentEncoding.mjs';
+import getHeaderValue from './getHeaderValue.mjs';
 
 export default (chunk, headers) => {
   assert(_.isPlainObject(headers));
+  const contentType = getHeaderValue(headers, 'content-type');
+  const contentEncoding = getHeaderValue(headers, 'content-encoding');
   if (!chunk
     || chunk.length === 0
-    || !/application\/(json|x-www-form-urlencoded)/i.test(headers['content-type'])
+    || !contentType
+    || !/application\/(json|x-www-form-urlencoded)\b/i.test(contentType)
   ) {
     return null;
   }
   try {
     const content = decodeContentEncoding(
       chunk,
-      headers['content-encoding'],
+      contentEncoding,
     );
-    if (/\/json/i.test(headers['content-type'])) {
+    if (/\/\bjson\b/i.test(contentType)) {
       return JSON.parse(content.toString());
     }
     return qs.parse(content.toString());
